073fd0fc35e5fb1c40181b8f58ec2b8e65243675
Martin Colwell <colwem@gmail.com>
2016-03-23 16:08:44 -0400

    WIP

---
+++ README.md
@@ -0,0 +1,12 @@
+Language
+
+```
+after wednesday days th > 70 and c are sunny
+after first day th > 70, before third day th > 80, each day ws < 10
+days before sunday th > 70
+good weather is th between 60 and 85 and conditions are sunny or partly cloudy
+golf weather is good weather
+```
+
+
+
--- index.js
+++ index.js
@@ -1,69 +1,159 @@
+#!/usr/bin/env node
 "use strict";
 
-var request = require('request');
-var urljoin = require('url-join');
-var _ = require('lodash');
-var async = require('async');
+let request = require('request');
+let urljoin = require('url-join');
+let _ = require('lodash');
+let program = require('commander');
+let fs = require('fs');
+let async = require('async');
+let path = require('path');
 
-const API_KEY = "4586cd9aa8c8b365";
+let config = require('./config.json');
+
+program
+  .version('1.0.0')
+  .arguments('blah')
+  .option('-C, --Celsius', 'Use celsius temperature scale')
+  .option('-F, --Fahrenheit', 'Use fahrenheit temperature scale')
+  .option('-f, --filter <s>', 'Filter expression')
+  .option('-o, --output <file>', 'File to output json response from wunderground api')
+  .option('-i, --input <file>', 'json file to use instead of wunderground api')
+  .parse(process.argv)
 
 
 class ApiUrl {
 
-  constructor() {
-    this.apiBaseUrl = "http://api.wunderground.com/api/";
+  constructor(api, config) {
+    this.api = api;
+    this.config = config;
     this._defaults = {
+      api: 'wunderground',
       city: 'arlington',
       state: 'ma',
       dataType: 'json',
-      apiKey: API_KEY,
-      method: 'conditions'
+      key: config.apis[api].key,
+      baseUrl: config.apis[api].url,
+      method: 'forecast10day'
     }
   }
 
-  getMethod() {
-    return this.method ? this.method : this._defaults.method;
-  }
-
-  getCity() {
-    return this.city ? this.city : this._defaults.city;
-  }
-
-  getState() {
-    return this.state ? this.state : this._defaults.state;
-  }
-
   getLocationUrl() {
-    return urljoin('q', this.getState(), this.getCity());
+    return urljoin('q', this.get('state'), this.get('city'));
   }
 
-  getMethod() {
-    return this.method ? this.method : this._defaults.method;
+  get(key) {
+    return this[key] ? this[key] : this._defaults[key];
   }
 
   getDataTypeUrl() {
-    let d = this.dataType ? this.dataType : this._defaults.dataType;
-    return '.' + d
+    return '.' + this.get('dataType');
   }
 
   url() {
-    return urljoin(this.apiBaseUrl, API_KEY, this.getMethod(), this.getLocationUrl()) + this.getDataTypeUrl();
+    if(this.api === 'wunderground') return this.wuUrl();
+    if(this.api === 'openWeatherMap') return this.owmUrl();
+  }
+
+  wuUrl() {
+    return urljoin(this.get('baseUrl'), this.get('key'), this.get('method'), this.getLocationUrl()) + this.getDataTypeUrl();
+  }
+
+  owmUrl() {
+    return urljoin(this.get('baseUrl'), this.get('key'), this.get('method'), this.getLocationUrl()) + this.getDataTypeUrl();
+  }
+}
+
+class Base {
+  constructor(f) {
+    if( ! f ) return;
+    this.filters = f.constructor === 'Array' ? f : [f];
+  }
+
+  process(w) {
+    console.log('Base.process');
+    _(this.filters).forEach(f => {
+      w = f.process(w);
+    }); 
+    return w;
+  }
+
+  days(w) { 
+    return w;
+  }
+}
+
+class EachDay extends Base {
+  
+}
+
+class Weekday extends Base {
+  process(w) {
+    w = super.process(w);
+    return _(this.days(w)).map(d => d.weekday);
+  }
+}
+
+class First extends Base{ 
+
+  process(w) {
+    console.log('First.process');
+    w = super.process(w);
+    return this.days(w)[0];
+  }
+}
+
+class Days extends Base {
+  constructor(filters) {
+    super(filters);
+  }
+
+  process(w) {
+    console.log('Days.process');
+    w = super.process(w); 
+    return this.days(w).length;
+  }
+}
+
+class Th extends Base {
+
+  constructor(low, high) {
+    super();
+    this.low = low;
+    this.high = high ? high : 200;
+  }
+
+  process(w) {
+    console.log('Th.process');
+    w = _(this.days(w)).filter(d => {
+      return d.high.fahrenheit > this.low && d.high.fahrenheit < this.high;
+    }).value(); 
+    debugger;
+    return w;
   }
 }
 
 let apiUrl = new ApiUrl();
-apiUrl.method = 'forecast';
-console.log("getting: ", apiUrl.url());
+apiUrl.key = config.apiKeys.openWeatherMap;
+apiUrl.method = 'forecast10day';
 
-function test() {
-  request.get(apiUrl.url(), function(err, response, body) {
+if( program.output ) {
+  request.get(apiUrl.url()).pipe(fs.createWriteStream(program.output));
+} else if( program.input ) {
+  run(require('./' + program.input));
+} else {
+  request.get(apiUrl.url(), (err, response, body) => {
     if(err) {
       console.log(err);
       throw err;
     }
-    debugger;
-    console.log(body);
+    run(body);
   });
 }
 
-test();
+function run(w) {
+  w = w.forecast.simpleforecast.forecastday;
+  let prog = new Days( new Th(40, 80));
+  console.log(prog.process(w));
+}
+
